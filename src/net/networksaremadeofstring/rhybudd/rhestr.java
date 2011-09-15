7cc4cff04fdc0c7d508e37eb45960f47b7125d58
Gareth Llewellyn <gareth@networksaremadeofstring.co.uk>
2011-09-15 21:59:56 +0100

    Initial experimentation with integrating the Zenoss API Java example

--- src/net/networksaremadeofstring/rhybudd/rhestr.java
+++ src/net/networksaremadeofstring/rhybudd/rhestr.java
@@ -2,12 +2,31 @@ package net.networksaremadeofstring.rhybudd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class rhestr extends Activity {
+public class rhestr extends Activity 
+{
+	ZenossAPI API = null;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        try
+    	{
+    		API = new ZenossAPI();
+    	}
+    	catch(Exception e)
+    	{
+    		
+    	}
+        
+        try {
+			Log.i("Devices", API.getDevices().toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
